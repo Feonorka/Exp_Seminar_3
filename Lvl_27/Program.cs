8fadf0b7e3cc41c1b4a7a0dc7a4ccb0825ac6686
Александр Ютишев <104940479+Feonorka@users.noreply.github.com>
2022-10-18 11:38:32 +0300

    Update Program.cs
    Выполнена 27 задача

--- Lvl_27/Program.cs
+++ Lvl_27/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int NumCounter(int number)
+{
+int res = default;
+while (number !=0)
+{
+res++;
+number = number / 10;
+}
+return res;
+}
+
+int NumSumm(int intNum)
+{
+int res = default;
+for (int i = 0; i <= NumCounter(num); i++)
+{
+res = res + (intNum % 10);
+intNum = intNum / 10;
+}
+return res;
+}
+int result = NumSumm(num);
+Console.WriteLine($"Сумма цифр в числе = {result}");
