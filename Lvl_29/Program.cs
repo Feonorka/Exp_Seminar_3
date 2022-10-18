d8c85c6c83ed4c3948cbc9fbe2f6ca3453a5b8e5
Александр Ютишев <104940479+Feonorka@users.noreply.github.com>
2022-10-18 11:39:48 +0300

    Update Program.cs
    Решена 29 задача

--- Lvl_29/Program.cs
+++ Lvl_29/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите количество элементов массива:");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[num];
+
+void PrintArray(int[] arr)
+{
+int count = arr.Length;
+for (int i = 0; i < count; i++)
+{
+if (i == count - 1) Console.Write($"{arr[i]} ");
+else Console.Write($"{arr[i]}, ");
+
+}
+
+}
+
+void FillArray(int[] col)
+{
+int lenght = col.Length;
+int index = 0;
+while (index < lenght)
+{
+col[index] = new Random().Next(1, 100);
+index++;
+}
+}
+
+FillArray(array);
+PrintArray(array);
